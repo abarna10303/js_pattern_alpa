4fe0835cc38c760aba5b570cb7b229696669966d
abarna10303 <abarnas361@gmail.com>
2022-12-15 13:56:35 +0530

    Alpahets

--- main.js
+++ main.js
@@ -5,7 +5,19 @@ for(i=0;i<5;i++)
     for(j=0;j<=i;j++)
     {
         document.write(alphabets[j]);
-        alphabets+1;
+    }
+    document.write("<br>");
+}
+document.write("<br>");
+document.write("<br>");
+var i,j;
+for(i=1;i<=5;i++)
+{
+    var alpha=65;
+    for(j=1;j<=i;j++)
+    {
+        document.write(String.fromCharCode(alpha));
+        alpha++;
     }
     document.write("<br>");
 }
